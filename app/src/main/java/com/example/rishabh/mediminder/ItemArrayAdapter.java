9ff8f98ca89bbf0db9ddf8cdc8ba043cb20e5766
Rishabh <rishabh0297@gmail.com>
2018-04-19 02:43:59 +0530

    first commit

---
+++ app/src/main/java/com/example/rishabh/mediminder/ItemArrayAdapter.java
@@ -0,0 +1,8 @@
+package com.example.rishabh.mediminder;
+
+/**
+ * Created by rishabh on 15/4/18.
+ */
+
+public class ItemArrayAdapter {
+}
---
+++ app/src/main/java/com/example/rishabh/mediminder/new_Reminder.java
@@ -0,0 +1,13 @@
+package com.example.rishabh.mediminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class new_Reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new__reminder);
+    }
+}
